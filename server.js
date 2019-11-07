46bcc91bcbc9cd26c52ebce219e4d6baf55f2f0a
Санджиев Савр Игоревич <sandzhiev_si@bdo.ru>
2019-11-07 12:35:02 +0300

    event listener on server 'request'

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ console.log(dt.myDateTime());
 
 const port = 8080;
 
-const server = http.createServer(function (req,res) {
+const server = http.createServer();
+
+server.on('request',function (req,res) {
   fs.readFile('demo1.html',function (err,data){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
@@ -20,5 +22,10 @@ const server = http.createServer(function (req,res) {
   });
   console.log(`Client app is serving locally on port ${port}`);
 });
+server.on('connection', function(){
+  console.log('connection event');
+})
 
-server.listen(port);
+server.listen(port, function(){
+  console.log('listening event');
+});
