701c434da00e7fd6e8f5365ed0c1f75ff6b08fb4
Санджиев Савр Игоревич <sandzhiev_si@bdo.ru>
2019-11-13 16:38:30 +0300

    trying do thing while working

--- server.js
+++ server.js
@@ -2,12 +2,43 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const fs = require('fs');
+const events =require('events');
 
-//include my own Modules
+// include my own Modules
 const dt = require('./dateAndTime');
 
-//test
+// play with process
+function cliCall(){
+  process.stdin.setEncoding('utf-8');
+  process.stdin.on('readable', function(){
+    var input = process.stdin.read();
+    if(input!==null){
+      process.stdout.write(input);
+      debugger;
+      if (input.trim()=='exit') {
+        process.exit();
+      }
+    }
+  });
+};
+cliCall();
+
+// test
 console.log(dt.myDateTime());
+
+// play with events
+var em = new events.EventEmiter();
+em.on()
+
+//test inside test
+const eventEmitter = require('events').EventEmiter;
+var counter = 0;
+setInterval(function(){
+  em.emit('timed', counter++);
+}, 3000);
+em.on('timed', function(){
+  console.log('timed' + data);
+});
 
 const port = 8080;
 
