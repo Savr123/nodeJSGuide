3a5a7cb711527a871d9b089aaf63d50abaae4e9d
Санджиев Савр Игоревич <sandzhiev_si@bdo.ru>
2019-11-14 19:18:11 +0300

    I am too lazy to commit that shit

--- server.js
+++ server.js
@@ -5,6 +5,45 @@ const https = require('https');
 const url = require('url');
 const fs = require('fs');
 const events =require('events');
+const util = require('util');
+  /* util.inherits() - позволяет одному конструктору унаследовать
+      методы прототипа другого - суперконструктора */
+
+function InputChecker (name, file){
+  this.name = name;
+  this.writeStream = fs.createWriteStream('./' + file + '.txt',{
+    'flags': 'a',
+    'encoding': 'utf-8',
+    'mode': 0o666
+  });
+}
+
+util.inherits(InputChecker, events.EventEmitter);
+InputChecker.prototype.check = function check(input){
+  // удаление лишних пропусков
+  let command = input.trim().substr(0,3);
+
+  // обработка команды
+  // команда wr: входные данные записываются в файл
+  if (command == 'wr:'){
+    this.emit('write', input.substr(3, input.length));
+
+    // Команда en: процесс завершается
+  }else if (command =='en:'){
+    this.emit('end');
+
+    // Эхо-вывод в стандартный выходной поток
+  }else{
+    this.emit('echo', input);
+  }
+};
+
+// Тестирование нового объекта и обработки событий
+let ic = new InputChecker('Shelley','output');
+var data = 'Абра-Кадабра Сим-Салабим'
+  ic.on('write', function(data) {   this.writeStream.write(data, 'utf8'); });
+  ic.on('echo', function( data) {   process.stdout.write(ic.name + ' wrote ' + data); });
+  ic.on('end', function() {   process.exit(); });
 
 // include my own Modules
 const dt = require('./dateAndTime');
@@ -29,16 +68,18 @@ cliCall();
 console.log(dt.myDateTime());
 
 // play with events
-var em = new events.EventEmiter();
-em.on()
+var em = new events.EventEmitter();
+// em.on()
 
 //test inside test
 const eventEmitter = require('events').EventEmiter;
 var counter = 0;
-setInterval(function(){
-  em.emit('timed', counter++);
-}, 3000);
-em.on('timed', function(){
+
+// setInterval(function(){
+//   em.emit('timed', counter++);
+// }, 3000);
+
+em.on('timed', function(data){
   console.log('timed' + data);
 });
 
@@ -60,9 +101,7 @@ process.stdin.on('readable', function(){
     //echo text
     process.stdout.write(input);
     var command = input.trim();
-    if (command == 'exit'){
-      process.exit(0);
-    }
+    ic.check(input)
   }
 });
 
